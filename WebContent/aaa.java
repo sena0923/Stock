c4d9174cedf05d3903a196db8568b7a2b2c3ddd5
Tyogome <ymg2465054@stu.o-hara.ac.jp>
2025-11-04 14:40:46 +0900

    zaza

---
+++ WebContent/aaa.java
@@ -0,0 +1,13 @@
+package wakarukun;
+
+public class Wakarukun {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Wakaru");
+
+		System.out.println("aaaaaa");
+
+	}
+
+}
