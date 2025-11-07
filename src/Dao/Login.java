6cf57d0752dba7e53abdbe6e6038a21486366ec3
sena0923 <ymg2465055@stu.o-hara.ac.jp>
2025-11-07 13:59:52 +0900

    11/7

---
+++ src/Dao/Login.java
@@ -0,0 +1,13 @@
+package Dao;
+
+public class Login extends Dao {
+
+	public List<Login> search(String keyword) throws Exception{
+		List<Login> list=new ArrayList<>();
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
