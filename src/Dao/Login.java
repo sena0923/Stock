a9a0104d9f866d9a738ea3e677c90f907ce60f06
sena0923 <ymg2465055@stu.o-hara.ac.jp>
2025-11-07 14:02:16 +0900

    117

--- src/Dao/Login.java
+++ src/Dao/Login.java
@@ -3,7 +3,7 @@ package Dao;
 public class Login extends Dao {
 
 	public List<Login> search(String keyword) throws Exception{
-		List<Login> list=new ArrayList<>();
+		List<Login> list=new ArrayList<>()
 
 	public static void main(String[] args) {
 
