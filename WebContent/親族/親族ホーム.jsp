c79f510c8f24fa37b9da502a28ce160132cdf906
Tyogome <ymg2465054@stu.o-hara.ac.jp>
2025-11-07 11:46:06 +0900

    さいとうせな

--- WebContent/親族/親族ホーム.jsp
+++ WebContent/親族/親族ホーム.jsp
@@ -4,11 +4,11 @@
 <main>
   <div class="mainmenu">
     <ul>
-      <li><a href="stock.jsp">備品管理</a></li>
-	  <li><a href="purchase.jsp">購入</a></li>
-	  <li><a href="notes.jsp">連絡帳</a></li>
+      <li><a href="#">備品管理</a></li>
+	  <li><a href="#">購入</a></li>
+	  <li><a href="#">連絡帳</a></li>
 	  <li><a href="親族/親族情報表示.jsp">ユーザー情報表示</a></li>
-	  <li><a href="親族/親族ログアウトt.jsp">ログアウト</a></li>
+	  <li><a href="親族/親族ログアウト.jsp">ログアウト</a></li>
     </ul>
   </div>
 </main>
--- src/Dao/Dao.java
+++ src/Dao/Dao.java
@@ -1,6 +1,23 @@
-import java.sql.*;
- 
-public class 商品Dao {
- 
-  private static final String URL
-  
+package Dao;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class Dao {
+
+	static DataSource ds;
+
+	public Connection getConnection() throws Exception {
+		// データソースがnullの場合
+		if (ds == null) {
+			// InitialContextを初期化
+			InitialContext ic = new InitialContext();
+			// データベースへ接続
+			ds = (DataSource) ic.lookup("java:/comp/env/jdbc/stock");
+		}
+		// データベースへのコネクションを返却
+		return ds.getConnection();
+	}
+}
